6378aac6133c68ba6a351ede5cfd6d9d4dc62e70
Victor Tuy <victor.tuy@hotmail.com>
2024-09-21 14:29:24 -0300

    Create chapter9.js

---
+++ json/chapter9.js
@@ -0,0 +1,395 @@
+let palavras = [
+    {
+        english: "excited",
+        portuguese: "animado(a)"
+    },
+    {
+        english: "i got",
+        portuguese: "eu consegui"
+    },
+    {
+        english: "curious",
+        portuguese: "curioso(a)"
+    },
+    {
+        english: "huh",
+        portuguese: "hã"
+    },
+    {
+        english: "got to be",
+        portuguese: "tem que ser"
+    },
+    {
+        english: "acting",
+        portuguese: "atuando"
+    },
+    {
+        english: "lately",
+        portuguese: "ultimamente"
+    },
+    {
+        english: "could be",
+        portuguese: "poderia ser"
+    },
+    {
+        english: "even",
+        portuguese: "até"
+    },
+    {
+        english: "don't you?",
+        portuguese: "não é?"
+    },
+    {
+        english: "guess",
+        portuguese: "adivinhar/acho"
+    },
+    {
+        english: "just have to",
+        portuguese: "só tem que"
+    },
+    {
+        english: "wait",
+        portuguese: "esperar"
+    },
+    {
+        english: "find out",
+        portuguese: "descobrir"
+    },
+    {
+        english: "easter",
+        portuguese: "páscoa"
+    },
+    {
+        english: "hunt",
+        portuguese: "caça"
+    },
+    {
+        english: "bunny trails",
+        portuguese: "trilhas de coelhinho"
+    },
+    {
+        english: "paw prints",
+        portuguese: "pegadas"
+    },
+    {
+        english: "ground",
+        portuguese: "chão"
+    },
+    {
+        english: "try out",
+        portuguese: "experimentar"
+    },
+    {
+        english: "spring",
+        portuguese: "primavera"
+    },
+    {
+        english: "warm",
+        portuguese: "quente"
+    },
+    {
+        english: "bloom",
+        portuguese: "florescer"
+    },
+    {
+        english: "summer",
+        portuguese: "verão"
+    },
+    {
+        english: "hot",
+        portuguese: "quente+"
+    },
+    {
+        english: "green grass",
+        portuguese: "grama verde"
+    },
+    {
+        english: "fall",
+        portuguese: "outono"
+    },
+    {
+        english: "changing leaves",
+        portuguese: "folhas mudando"
+    },
+    {
+        english: "bare trees",
+        portuguese: "árvores nuas"
+    },
+    {
+        english: "article of clothing",
+        portuguese: "peça de roupa"
+    },
+    {
+        english: "t-shirt",
+        portuguese: "camiseta"
+    },
+    {
+        english: "shirt",
+        portuguese: "camisa"
+    },
+    {
+        english: "jeans",
+        portuguese: "jeans"
+    },
+    {
+        english: "skirt",
+        portuguese: "saia"
+    },
+    {
+        english: "coat",
+        portuguese: "casaco"
+    },
+    {
+        english: "sweater",
+        portuguese: "suéter"
+    },
+    {
+        english: "socks",
+        portuguese: "meias"
+    },
+    {
+        english: "shoes",
+        portuguese: "sapatos"
+    },
+    {
+        english: "shorts",
+        portuguese: "bermuda"
+    },
+    {
+        english: "hat/cap",
+        portuguese: "chapéu/boné"
+    },
+    {
+        english: "pants",
+        portuguese: "calças"
+    },
+    {
+        english: "dress",
+        portuguese: "vestido"
+    },
+    {
+        english: "tank top",
+        portuguese: "regata"
+    },
+    {
+        english: "jacket",
+        portuguese: "jaqueta"
+    },
+    {
+        english: "sweatshirt",
+        portuguese: "moletom"
+    },
+    {
+        english: "beanie",
+        portuguese: "gorro"
+    },
+    {
+        english: "boots",
+        portuguese: "botas"
+    },
+    {
+        english: "gloves",
+        portuguese: "luvas"
+    },
+    {
+        english: "sandals",
+        portuguese: "sandálias"
+    },
+    {
+        english: "flip-flops",
+        portuguese: "chinelo"
+    },
+    {
+        english: "scarf",
+        portuguese: "cachecol"
+    },
+    {
+        english: "sundress",
+        portuguese: "vestido de verão"
+    },
+    {
+        english: "turtleneck",
+        portuguese: "gola alta"
+    },
+    {
+        english: "snow gear",
+        portuguese: "equipamento de neve"
+    },
+    {
+        english: "polo shirt",
+        portuguese: "camisa polo"
+    },
+    {
+        english: "bathing suit",
+        portuguese: "maiô"
+    },
+    {
+        english: "sweatsuit",
+        portuguese: "terninho de moletom"
+    },
+    {
+        english: "mini-skirt",
+        portuguese: "mini-saia"
+    },
+    {
+        english: "holiday",
+        portuguese: "feriado"
+    },
+    {
+        english: "thanksgiving",
+        portuguese: "ação de graças"
+    },
+    {
+        english: "st. patrick's day",
+        portuguese: "dia de são patrício"
+    },
+    {
+        english: "eve",
+        portuguese: "véspera"
+    },
+    {
+        english: "turkey",
+        portuguese: "peru"
+    },
+    {
+        english: "ham",
+        portuguese: "presunto"
+      },
+      {
+        english: "smoked",
+        portuguese: "defumado"
+      },
+      {
+        english: "once a week/month/year",
+        portuguese: "uma vez por semana/mês/ano"
+      },
+      {
+        english: "twice",
+        portuguese: "duas vezes"
+      },
+      {
+        english: "three... times",
+        portuguese: "três... vezes"
+      },
+      {
+        english: "always",
+        portuguese: "sempre (100%)"
+      },
+      {
+        english: "almost always",
+        portuguese: "quase sempre (80%)"
+      },
+      {
+        english: "usually",
+        portuguese: "geralmente (70%)"
+      },
+      {
+        english: "often",
+        portuguese: "frequentemente (60%)"
+      },
+      {
+        english: "sometimes",
+        portuguese: "às vezes (50%)"
+      },
+      {
+        english: "occasionally",
+        portuguese: "ocasionalmente (40%)"
+      },
+      {
+        english: "seldom/rarely",
+        portuguese: "raramente (30%)"
+      },
+      {
+        english: "hardly ever",
+        portuguese: "quase nunca (20%)"
+      },
+      {
+        english: "almost never",
+        portuguese: "quase nunca (10%)"
+      },
+      {
+        english: "never",
+        portuguese: "nunca (0%)"
+      },
+      {
+        english: "exchange",
+        portuguese: "troca/intercâmbio"
+      },
+      {
+        english: "exchange gifts",
+        portuguese: "trocar presentes"
+      },
+      {
+        english: "bring",
+        portuguese: "trazer"
+      },
+      {
+        english: "fun",
+        portuguese: "diversão"
+      },
+      {
+        english: "as",
+        portuguese: "como"
+      },
+      {
+        english: "wish each other",
+        portuguese: "desejar um ao outro"
+      },
+      {
+        english: "shines",
+        portuguese: "brilha"
+      },
+      {
+        english: "abroad",
+        portuguese: "no exterior"
+      },
+      {
+        english: "sunny",
+        portuguese: "ensolarado"
+      },
+      {
+        english: "thunderstorms",
+        portuguese: "tempestades"
+      },
+      {
+        english: "partly cloudy",
+        portuguese: "parcialmente nublado"
+      },
+      {
+        english: "rain",
+        portuguese: "chuva"
+      },
+      {
+        english: "mostly cloudy",
+        portuguese: "maioria nublada"
+      },
+      {
+        english: "rain and snow/freezing rain",
+        portuguese: "chuva e neve/chuva congelante"
+      },
+      {
+        english: "snow",
+        portuguese: "neve"
+      },
+      {
+        english: "rain showers",
+        portuguese: "chuvas"
+      },
+      {
+        english: "windy",
+        portuguese: "ventoso"
+      },
+      {
+        english: "mainly",
+        portuguese: "principalmente"
+      },
+      {
+        english: "chilly",
+        portuguese: "fresquinho"
+      },
+      {
+        english: "humid",
+        portuguese: "úmido"
+      }
+      
+]
